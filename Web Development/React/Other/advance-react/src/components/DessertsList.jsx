f56b450ef14d2d1745a3a42530fbc5abc740a021
devmoatassem <moatassemsindhu@gmail.com>
2023-10-19 22:29:51 +0500

    sorted and filter method used

--- Web Development/React/Other/advance-react/src/components/DessertsList.jsx
+++ Web Development/React/Other/advance-react/src/components/DessertsList.jsx
@@ -1,11 +1,16 @@
 function DessertsList(props) {
-    console.log(props.data)
-    const sortedList = props.data.map(desert => {
+    const lowCalories = props.data.filter(desert =>{
+        return desert.calories<500
+    }).sort((a,b) =>{
+        return a.calories - b.calories
+    })
+    console.log(lowCalories)
+    const List = lowCalories.map(desert => {
         return ( <li>{`${desert.name} - ${desert.calories} cal`}</li> )
     })
     return (
         <ul>
-            {sortedList}
+            {List}
         </ul>
     )
   }
