87fea7d94ecb238c6cbeaa8960dfd60631eed55c
devmoatassem <moatassemsindhu@gmail.com>
2023-10-20 09:59:57 +0500

    list manipulation

--- Web Development/React/Other/advance-react/src/components/DessertsList.jsx
+++ Web Development/React/Other/advance-react/src/components/DessertsList.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react"
+
+
+
 function DessertsList(props) {
     const lowCalories = props.data.filter(desert =>{
         return desert.calories<500
@@ -6,12 +10,26 @@ function DessertsList(props) {
     })
     console.log(lowCalories)
     const List = lowCalories.map(desert => {
-        return ( <li>{`${desert.name} - ${desert.calories} cal`}</li> )
+        return ( <li>{desert.name} - {desert.calories} cal</li> )
     })
+
+    const [flist,setFlist] = useState(List)
+
+    const revserOrder =()=>{
+        setFlist([...flist].reverse());
+    }
+    // Spread Syntax ...: The ... before flist is used to spread the elements of the flist array into the new array. It essentially takes each element of the flist array and adds them to the new array.
+
+    // So, when you use [...flist], you're creating a new array that has the same elements as the flist array, and it's not just a reference to the same array. This is useful when you want to work with a copy of an array, leaving the original array unchanged. Here's an example:
     return (
+        <>
         <ul>
-            {List}
+            {flist}
         </ul>
+        <button onClick={revserOrder}>Reverse Order</button>
+        </>
+        
+
     )
   }
   
