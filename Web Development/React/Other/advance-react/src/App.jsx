287d4863c67c6b5fc20ca590c7e92d85ba3f19de
devmoatassem <moatassemsindhu@gmail.com>
2023-10-25 18:49:09 +0500

    theme switcher

--- Web Development/React/Other/advance-react/src/App.jsx
+++ Web Development/React/Other/advance-react/src/App.jsx
@@ -3,45 +3,51 @@ import { useContext } from 'react';
 import FormMani from './components/FormMani';
 import Switch from './components/Switch';
 import ThemeContext from './context/ThemeContext';
+import { RadioGroup,RadioOption } from './components/RadioGroup';
 
 
 
 
-
-const desserts = [
-  {
-    name: "Chocolate Cake",
-    calories: 400,
-    createdAt: "2022-09-01",
-  },
-  {
-    name: "Ice Cream",
-    calories: 200,
-    createdAt: "2022-01-02",
-  },
-  {
-    name: "Tiramisu",
-    calories: 300,
-    createdAt: "2021-10-03",
-  },
-  {
-    name: "Cheesecake",
-    calories: 600,
-    createdAt: "2022-01-04",
-  },
-];
+// const desserts = [
+//   {
+//     name: "Chocolate Cake",
+//     calories: 400,
+//     createdAt: "2022-09-01",
+//   },
+//   {
+//     name: "Ice Cream",
+//     calories: 200,
+//     createdAt: "2022-01-02",
+//   },
+//   {
+//     name: "Tiramisu",
+//     calories: 300,
+//     createdAt: "2021-10-03",
+//   },
+//   {
+//     name: "Cheesecake",
+//     calories: 600,
+//     createdAt: "2022-01-04",
+//   },
+// ];
 
 
 function App() {
   const {theme, setTheme} = useContext(ThemeContext)
 
   return (
-    <div id='root'
-    style={{backgroundColor :theme? "black":"white", color:theme?"white":"black"}}>
-      <Switch/>
-    <FormMani/>
+    // <div id='root'
+    // style={{backgroundColor :theme? "black":"white", color:theme?"white":"black"}}>
+    //   <Switch/>
+    // <FormMani/>
    
-    </div>
+    // </div>
+    <RadioGroup onChange={setSelected} selected={selected}> 
+    <RadioOption value="social_media">Social Media</RadioOption> 
+    <RadioOption value="friends">Friends</RadioOption> 
+    <RadioOption value="advertising">Advertising</RadioOption> 
+    <RadioOption value="other">Other</RadioOption>
+    </RadioGroup> 
   )
 }
 
