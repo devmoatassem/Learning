3fcce8c5d435b1094525cca8815faccec3ea70b9
devmoatassem <moatassemsindhu@gmail.com>
2023-12-08 11:51:29 +0500

    redering different view in express js

--- Web Development/ExpressJs/Express generator/express-app/app.js
+++ Web Development/ExpressJs/Express generator/express-app/app.js
@@ -13,11 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/static",express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
