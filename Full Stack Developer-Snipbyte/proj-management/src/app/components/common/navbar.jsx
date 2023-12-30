f7d520bc9aeae66f52b466cf48562a0535b1f40b
devmoatassem <moatassemsindhu@gmail.com>
2023-12-30 23:23:43 +0500

    navbar responsive done

--- Full Stack Developer-Snipbyte/proj-management/src/app/components/common/navbar.jsx
+++ Full Stack Developer-Snipbyte/proj-management/src/app/components/common/navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Button from './button'
-
-
+import { IoMenu } from "react-icons/io5";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
     const navLinks = [
@@ -30,9 +31,12 @@ const Navbar = () => {
 
     const navLinksList = navLinks.map((item, index) => {
         return (
-            <Link href={item.link} key={index} className='text-white hover:text-btpurple hover:underline'>{item.title}</Link>
+            <Link href={item.link} key={index} className='hover:bg-bggrey  md:hover:text-btpurple hover:underline'>{item.title}</Link>
         )
     })
+
+    const [nav, setNav] = useState(false);
+
 
     return (
         <nav className='absolute top-0 bg-transparent w-full z-50'>
@@ -40,12 +44,21 @@ const Navbar = () => {
                 <div className='text-xl font-extrabold text-white'>
                     Logo Lorem
                 </div>
-                <div className='hidden sm:flex gap-6 font-inter'>
+                <div className='hidden md:flex gap-6 font-inter text-white'>
                     {navLinksList}
                 </div>
-                <Button button='Login' customClass='max-w-32 bg-white text-black' />
-            </div>
+                <Button button='Login' customClass='hidden md:block max-w-32 bg-white text-black' />
+                <button className='md:hidden text-white text-2xl' onClick={() => setNav(!nav)}><IoMenu /></button>
 
+                <div className={nav ? "block" : "hidden"}>
+                    <div className='w-screen h-screen bg-white absolute inset-0 p-5'>
+                        <button onClick={() => setNav(!nav)} className='text-2xl text-black absolute right-10 top-10'><AiOutlineClose /></button>
+                        <div className='text-black flex flex-col items-center justify-center gap-20 py-32'>
+                            {navLinksList}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
